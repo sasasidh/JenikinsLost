f7ff79ba2c965078d923ec5894e8678da8c7ff51
sasasidh <110666446+sasasidh@users.noreply.github.com>
2023-03-27 13:15:06 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,5 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Poll SCM lets go"); 
+        System.out.println("Poll SCM : Global COnfig changes to git.exe"); 
     }
 }
