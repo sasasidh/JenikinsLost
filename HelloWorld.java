d3779d0c425cfbf024106dce08ba5bc2f70c7bfb
sasasidh <110666446+sasasidh@users.noreply.github.com>
2023-03-28 02:25:12 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,5 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Webhook : ngrok tunneling , webhook placeholder changed"); 
+        System.out.println("Webhook : ngrok tunneling , webhook URL added /github-webhook/"); 
     }
 }
